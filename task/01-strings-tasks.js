ce437c8441d4b79c2b94c511e14de5e9cb72d15b
OlgaPlatonenko <platonenkoolga@gmail.com>
2021-04-07 11:35:38 +0300

    done parse name

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -68,8 +68,9 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(value) {
-    throw new Error('Not implemented');
+function extractNameFromTemplate(value) {  
+  
+   return value.substring(value.indexOf(' ')+1,value.length-1);
 }
 
 
